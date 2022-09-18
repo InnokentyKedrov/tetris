85d7eeef85460d02f9b448ca01f7b53d6a996891
Andrey <kedr_club@mail.ru>
2022-09-18 16:03:06 +0300

    init: adds files

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Controller } from './modules/controller.js';
+import { Game } from './modules/game.js';
+import { View } from './modules/view.js';
+
+export const SIZE_BLOCK = 30;
+export const COLUMNS = 10;
+export const ROWS = 20;
+
+const game = new Game();
+const view = new View(document.querySelector('.container'));
+const controller = new Controller(game, view);
+
+controller.init('Enter');
---
+++ modules/controller.js
@@ -0,0 +1,50 @@
+export class Controller {
+  constructor (game, view) {
+    this.game = game;
+    this.view = view;
+  }
+
+  init(codeKey) {
+    const startText = document.querySelector('.start-text');
+
+    window.addEventListener('keydown', event => {
+      if (event.code === codeKey) {
+        startText.remove();
+        this.view.init();
+        this.start();
+      }
+    })
+  }
+
+  start() {
+    this.view.showArea(this.game.viewArea);
+
+    setInterval(() => {
+      this.game.moveDown();
+      this.view.showArea(this.game.viewArea);
+    }, 100);
+
+    window.addEventListener('keydown', event => {
+      const key = event.code;
+    
+      switch (key) {
+        case 'ArrowLeft':
+          this.game.moveLeft();
+          this.view.showArea(this.game.viewArea);
+        break;
+        case 'ArrowRight':
+          this.game.moveRight();
+          this.view.showArea(this.game.viewArea);
+        break;
+        case 'ArrowDown':
+          this.game.moveDown();
+          this.view.showArea(this.game.viewArea);
+        break;
+        case 'ArrowUp':
+          this.game.rotateTetromino();
+          this.view.showArea(this.game.viewArea);
+        break;
+      }
+    });
+  }
+}
